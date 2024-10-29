7b514f95adaeec602ba5181be1d505f749f12afc
k3lly003 <ntirukelly@gmail.com>
2024-10-29 17:18:39 +0200

    ch-project-setup

--- igire-web/tailwind.config.js
+++ igire-web/tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        'plex-sans': ['"IBM Plex Sans"', 'sans-serif'],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
