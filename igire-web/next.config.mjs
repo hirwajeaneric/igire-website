be2b28ee40e78af622a564627e4b8fe289751496
k3lly003 <ntirukelly@gmail.com>
2024-11-04 02:12:56 +0200

    main

--- igire-web/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    images: {
-        domains: ['shecancode-website-dev.vercel.app'],
-    },
-};
-
-export default nextConfig;
