84431f10bd36f6af56433b4b009dd8963d3bba6d
HildeNancy25 <hildenancyisingizwe@gmail.com>
2024-12-05 21:12:32 +0200

    login API integration

--- igire-web/next.config.mjs
+++ igire-web/next.config.mjs
@@ -1,10 +1,39 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-        domains: ['shecancode-website-dev.vercel.app', 'res.cloudinary.com', 'media.licdn.com', 'firebasestorage.googleapis.com'],
-    },
-  
+  reactStrictMode: true,
+  images: {
+    domains: [
+      "shecancode-website-dev.vercel.app",
+      "res.cloudinary.com",
+      "media.licdn.com",
+      "firebasestorage.googleapis.com",
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://iro-employee-bn.onrender.com/api/:path*",
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, PATCH, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Origin, Content-Type, Accept",
+          },
+        ],
+      },
+    ];
+  },
 };
-
 export default nextConfig;
